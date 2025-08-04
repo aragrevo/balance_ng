06ac37fe14766d356a36e21ba6c223d074c84488
Eduardo Vergara� <eduver_san@hotmail.com>
2025-08-04 13:42:14 +0100

    feat(app): add loading spinner during navigation
    Implement router event subscription to show/hide loading spinner during navigation. The spinner is now conditionally rendered based on the navigation state.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { SpinnerComponent } from '@app/components/spinner/spinner';
 
 @Component({
@@ -10,4 +10,16 @@ import { SpinnerComponent } from '@app/components/spinner/spinner';
 })
 export class App {
   protected readonly title = signal('balance-app');
+  protected readonly isLoading = signal(false);
+  private readonly router = inject(Router);
+
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isLoading.set(true);
+      } else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        this.isLoading.set(false);
+      }
+    })
+  }
 }
