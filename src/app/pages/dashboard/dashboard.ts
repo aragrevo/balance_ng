168e652dbc793b9041a43515decc46894815bc24
Eduardo Vergara� <eduver_san@hotmail.com>
2025-08-04 13:43:50 +0100

    feat(dashboard): add user avatar and sign out functionality
    - Import UserAvatarComponent and add to dashboard imports
    - Update auth service to use user$ BehaviorSubject and handle sign out
    - Implement sign out method in dashboard component
    - Update dashboard template with new header layout and sign out button

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,16 +1,22 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { UserAvatarComponent } from '@app/components/user-avatar/user-avatar';
 import { AuthService } from '@app/services/auth.service';
 
 @Component({
   selector: 'dashboard',
-  imports: [],
+  imports: [UserAvatarComponent],
   templateUrl: './dashboard.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
   private readonly authService = inject(AuthService);
-  user = toSignal(this.authService.user, {
+  user = toSignal(this.authService.user$, {
     requireSync: true,
   });
+
+  signOut() {
+    this.authService.signOut();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,23 +11,24 @@ export class AuthService {
   private readonly supabaseSvc = inject(SupabaseService)
   private readonly router = inject(Router)
 
-  user = new BehaviorSubject<User | null>(null);
+  user$ = new BehaviorSubject<User | null>(null);
 
   constructor() {
     this.supabaseSvc.supabase.auth.onAuthStateChange((event, session) => {
       console.log(event);
       console.log(session);
 
-      if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
-        this.user.next({
+      if (session && (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED' || event === 'INITIAL_SESSION')) {
+        this.user$.next({
           id: session!.user.id,
           name: session!.user.user_metadata['name'],
           avatar: session!.user.user_metadata['avatar_url'],
         });
         this.router.navigate(['/dashboard']);
-      } else {
-        this.user.next(null);
+        return;
       }
+      this.user$.next(null);
+
     });
   }
 
@@ -37,6 +38,18 @@ export class AuthService {
       throw res.error
     }
     return res.data
+  }
+
+  async signOut() {
+    const res = await this.supabaseSvc.supabase.auth.signOut({
+      scope: 'global',
+    })
+    if (res.error) {
+      throw res.error
+    }
+    this.user$.next(null);
+    this.router.navigate(['/']);
+    return res.error === null
   }
 
 }
