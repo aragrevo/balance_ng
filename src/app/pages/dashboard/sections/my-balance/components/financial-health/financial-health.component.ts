75d653bdc7818b3fe7fc16b6590acbd3f1c4ef71
Eduardo Vergara� <eduver_san@hotmail.com>
2025-08-07 14:34:32 +0100

    feat(dashboard): add financial health component to display balance and transactions
    Implement a new component to show financial health metrics including EUR and COP balances, incomes, and expenses. The component displays data in a visually appealing format with gradient backgrounds and interactive elements.

---
+++ src/app/pages/dashboard/sections/my-balance/components/financial-health/financial-health.component.ts
@@ -0,0 +1,51 @@
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { MoneyTypes } from '@app/models/money-types.enum';
+import { Transaction } from '@app/models/transaction.model';
+import { BalanceData } from '@app/models/user.model';
+
+@Component({
+  selector: 'financial-health',
+  imports: [],
+  templateUrl: './financial-health.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+    .bg-red-500-20 {
+      background-color: rgba(251,44,54,0.2);
+    }
+    .bg-indigo-500-20 {
+      background-color: rgba(97,95,255,0.2)
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FinancialHealthComponent {
+  balance = input.required<Record<MoneyTypes, BalanceData> | null>();
+  transactions = input.required<Transaction[]>();
+  incomes = input.required<Transaction[]>();
+
+  protected readonly eurBalance = computed(() => this.balance()?.[MoneyTypes.EUR]);
+  protected readonly copBalance = computed(() => this.balance()?.[MoneyTypes.COP]);
+  protected readonly eurTransactions = computed(() => this.transactions()?.filter((t) => t.money === MoneyTypes.EUR));
+  protected readonly copTransactions = computed(() => this.transactions()?.filter((t) => t.money === MoneyTypes.COP));
+  protected readonly amounts = computed(() => {
+    return [
+      {
+        type: "EUR Incomes",
+        values:
+          this.incomes()?.filter((t) => t.money === MoneyTypes.EUR).map((t) => t.cost) ||
+          [],
+      },
+      { type: "EUR Expenses", values: this.eurTransactions()?.map((t) => t.cost) || [] },
+      {
+        type: "COP Incomes",
+        values:
+          this.incomes()?.filter((t) => t.money === MoneyTypes.COP).map((t) => t.cost) ||
+          [],
+      },
+      { type: "COP Expenses", values: this.copTransactions()?.map((t) => t.cost) || [] },
+    ];
+  });
+
+}
