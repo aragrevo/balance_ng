0c8ae3de0f5fc99d5dd26617b3ecca92fd0ebc22
Eduardo Vergara� <eduver_san@hotmail.com>
2025-08-07 18:38:19 +0100

    feat(admin): add category model and expense categories service
    Introduce Category interface model and AdminService with method to fetch expense categories from Supabase. The service includes authentication check and error handling.

---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,32 @@
+import { inject, Injectable } from '@angular/core';
+import { Transaction } from '@app/models/transaction.model';
+import { AuthService } from './auth.service';
+import { SupabaseService } from './supabase.service';
+import { Category } from '@app/models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  private readonly supabaseSvc = inject(SupabaseService)
+  private readonly authSvc = inject(AuthService)
+
+
+  async getExpenseCategories(): Promise<Category[]> {
+    const user = this.authSvc.user$.value
+    if (!user) {
+      throw new Error('User not authenticated');
+    }
+    const { data, error } = await this.supabaseSvc.supabase.from("categories").select()
+      .filter("type", "eq", "expense")
+      .order("name", { ascending: true });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data;
+  }
+
+}
