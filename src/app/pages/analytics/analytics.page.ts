295340f476981eb41dde0c6c2c3bed2dd5c20774
Eduardo Vergara� <eduver_san@hotmail.com>
2025-08-08 18:09:47 +0100

    feat(analytics): implement analytics page layout and data loading
    Add header, title section and content containers with placeholders for charts
    Implement balance data loading functionality using BalanceService

--- src/app/pages/analytics/analytics.page.ts
+++ src/app/pages/analytics/analytics.page.ts
@@ -1,9 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { afterNextRender, ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { MoneySelectorComponent } from '@app/components/money-selector/money-selector';
+import { UserAvatarComponent } from '@app/components/user-avatar/user-avatar';
 import { DashboardLayoutComponent } from '@app/layouts/dashboard-layout';
+import { getStartOfMonth, getEndOfMonth } from '@app/lib/date';
+import { MoneyTypes } from '@app/models/money-types.enum';
+import { BalanceData } from '@app/models/user.model';
+import { BalanceService } from '@app/services/balance.service';
+import { DistributionOverviewComponent } from './sections/distribution-overview/distribution-overview';
 
 @Component({
   selector: 'analytics',
-  imports: [DashboardLayoutComponent],
+  imports: [DashboardLayoutComponent, UserAvatarComponent, MoneySelectorComponent, DistributionOverviewComponent],
   templateUrl: './analytics.page.html',
   styles: `
     :host {
@@ -12,4 +19,31 @@ import { DashboardLayoutComponent } from '@app/layouts/dashboard-layout';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AnalyticsComponent { }
+export class AnalyticsComponent {
+  balance = signal<Record<MoneyTypes, BalanceData> | null>(null);
+
+  private readonly balanceService = inject(BalanceService);
+  constructor() {
+    afterNextRender(() => {
+      this.loadData();
+    });
+  }
+
+  private loadData() {
+    this.loadBalance();
+    // this.loadTransactions();
+    // this.loadIncomes();
+  }
+
+  private loadBalance() {
+    const startDate = getStartOfMonth().toISOString();
+    const endDate = getEndOfMonth().toISOString();
+    this.balanceService.getBalance(startDate, endDate)
+      .then(res => {
+        this.balance.set(res);
+      }).catch(err => {
+        console.log(err);
+      })
+
+  }
+}
